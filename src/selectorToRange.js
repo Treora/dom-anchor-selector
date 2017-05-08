f0b52c416f1b43e6af9ccb4b4d73a1df8e43472e
Gerben <gerben@treora.com>
2017-05-07 18:17:07 -0700

    Support multiple selectors.
    Currently hard-coding the simple strategy of returning the first one that
    works.

--- src/selectorToRange.js
+++ src/selectorToRange.js
@@ -1,13 +1,41 @@
 import * as textQuote from 'dom-anchor-text-quote'
 
-export default function selectorToRange(selector, root=window.document.body) {
+
+export default function selectorToRange(selector) {
+    const selectors = Array.isArray(selector) ? selector : [selector]
+    const range = resolveSelectors(selectors)
+    return range
+}
+
+function resolveSelectors(selectors) {
+    const strategy = whateverWorks
+    return strategy(selectors)
+}
+
+function resolveSelector(selector) {
+    const root = window.document.body
     switch (selector.type) {
         case 'TextQuoteSelector':
             return textQuote.toRange(root, selector)
         default:
             throw new Error(`Unknown selector type: '${selector.type}'`)
     }
-    // TODO support more selector types
-    // TODO support multiple selectors
-    // TODO support refined selectors
+}
+
+function whateverWorks(selectors) {
+    const errors = []
+    for (let selectorIndex = 0; selectorIndex < selectors.length; selectorIndex++) {
+        const selector = selectors[selectorIndex]
+        try {
+            const range = resolveSelector(selector)
+            // We successfully found the fragment, return it.
+            return range
+        } catch (err) {
+            // Anchoring this selector did not succeed, try the next alternative.
+            errors.push(err)
+            continue
+        }
+    }
+    // None of them worked, bummer.
+    throw new Error(`Could not anchor any selector; errors: ${errors.map(e => `\n- ${e}`)}`)
 }
